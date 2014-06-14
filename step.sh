0c65dd1d3c8fab758b7afe972a949736c6077f74
Viktor Benei <viktor.benei@gmail.com>
2014-06-14 13:41:14 +0200

    option to whether or not to use 'sudo'. Default is 'do use sudo'.

--- step.sh
+++ step.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 if [ ! -n "$SECURE_DELETE_PATH" ]; then
-	echo '[!] Input $SECURE_DELETE_PATH missing!'
-	exit 1
+  echo '[!] Input $SECURE_DELETE_PATH missing!'
+  exit 1
+fi
+
+is_do_with_sudo=1 # use sudo? default is yes
+if [[ -n "$SECURE_DELETE_WITHSUDO" && "$SECURE_DELETE_WITHSUDO" == 'false' ]]; then
+  is_do_with_sudo=0
 fi
 
 function print_and_do_command {
@@ -10,5 +15,12 @@ function print_and_do_command {
   $@
 }
 
-print_and_do_command rm -rfP "$SECURE_DELETE_PATH"
+echo "Removing $SECURE_DELETE_PATH ..."
+if [ $is_do_with_sudo -eq 1 ]; then
+  echo " (i) Using sudo"
+  print_and_do_command sudo rm -rfP $SECURE_DELETE_PATH
+else
+  echo " (i) NOT using sudo"
+  print_and_do_command rm -rfP $SECURE_DELETE_PATH
+fi
 exit $?
