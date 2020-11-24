43e6dbaf2a0a8c34fb42863d5edf622d6291d116
Daniel Serleg <daniel.serleg@bitrise.io>
2020-11-24 17:23:41 +0100

    Integrate the OS check

--- step.sh
+++ step.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ ! -n "$path" ]; then
+if [ ! -n "$path_v" ]; then
   echo '[!] Input $path missing!'
   exit 1
 fi
 
 # this expansion is required for paths with ~
 #  more information: http://stackoverflow.com/questions/3963716/how-to-manually-expand-a-special-variable-ex-tilde-in-bash
-eval expanded_target_path="$path"
+eval expanded_target_path="$path_v"
 
 is_do_with_sudo=1 # use sudo? default is yes
 if [[ -n "$with_sudo" && "$with_sudo" == 'false' ]]; then
@@ -30,17 +30,18 @@ function print_and_do_command {
 
 determine_os_type
 case "${os_type}" in
-    Linux*)     os_type=Linux;;
-    Mac*)       os_type=Mac;;
-    *)          os_type="UNKNOWN:${unameOut}"
+    Linux*)     os_specific_switches="";;
+    Mac*)       os_specific_switches="-P";;
+    *)          os_specific_switches=""
 esac
 
+echo "OS type: $determine_os_type"
 echo "Removing $expanded_target_path ..."
 if [ $is_do_with_sudo -eq 1 ]; then
   echo " (i) Using sudo"
-  print_and_do_command sudo rm -rfP "$expanded_target_path"
+  print_and_do_command $("sudo rm -rf ${os_specific_switches} ${expanded_target_path}")
 else
   echo " (i) NOT using sudo"
-  print_and_do_command rm -rfP "$expanded_target_path"
+  print_and_do_command $("rm -rf ${os_specific_switches} ${expanded_target_path}")
 fi
 exit $?
